035548c4468d49a961324a3c8ab6b3f33c0b1561
rammohany <rammohany@HDLRAMMOHANY.Virtusa.com>
2018-03-15 11:59:02 +0530

    Organized imports

--- src/main/java/com/virtusa/comments/controller/CommentController.java
+++ src/main/java/com/virtusa/comments/controller/CommentController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
